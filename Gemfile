e687e7871c16f6205d4e6ab22387c94965bda17a
shuta0417 <shuta19870417@gmail.com>
2020-11-24 16:59:33 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ end
 
 gem 'devise'
 gem 'pry-rails'
+
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  private
+  
+  def
+   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
