41e075f1d38a618c947e138b04b1587078ada01f
shuta0417 <shuta19870417@gmail.com>
2020-12-07 17:32:05 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "items#index"
+  root to: "items#index"
 
   resources :items do
     resources :purchase_records, only: [:index, :create]
